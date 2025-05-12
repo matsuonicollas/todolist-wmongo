e2eceb0211534da70ed928ee0c02d4159b6cac5e
Nicollas Matsuo <144185296+matsuonicollas@users.noreply.github.com>
2025-05-12 12:11:14 +0000

    createresolvido

--- requests/todo-create.sh
+++ requests/todo-create.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 curl -X POST http://localhost:3000/api/todos \
 -H "Content-Type: application/json" \
--H "Authorization: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MjE1Zjg5Y2Y5ZjViZmFjMGY4NDFmMCIsImlhdCI6MTc0NzAyMjM1OSwiZXhwIjoxNzQ3MDI1OTU5fQ.Ht52k_LysQtV8Xv-Jcl-LUoVbwllit5FlycS5UUrOTw" \
+-H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MjE1Zjg5Y2Y5ZjViZmFjMGY4NDFmMCIsImlhdCI6MTc0NzA1MTY4NCwiZXhwIjoxNzQ3MDU1Mjg0fQ.MZKoYmzzz9uUQVm3dY9Cqfq-4A4TyzE3Ot1WsONTPZQ" \
 -d '{
   "title": "Novo To-Do",
   "description": "Descrição do to-do"
--- requests/todo-update.sh
+++ requests/todo-update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Substitua <ID_DO_TODO> pelo ID real do to-do que deseja atualizar.
-curl -X PUT http://localhost:3000/api/todos/68216079ad2983facda906a3 \
+curl -X PUT http://localhost:3000/api/todos/6821e4b7e3d16219b2faad27 \
 -H "Content-Type: application/json" \
--H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MjE1Zjg5Y2Y5ZjViZmFjMGY4NDFmMCIsImlhdCI6MTc0NzAyMTc3NCwiZXhwIjoxNzQ3MDI1Mzc0fQ.eJl-X3imZC1atWc8b2Dms0WOJEBe6sutmQNlk-B8PS0" \
+-H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MjE1Zjg5Y2Y5ZjViZmFjMGY4NDFmMCIsImlhdCI6MTc0NzA1MTY4NCwiZXhwIjoxNzQ3MDU1Mjg0fQ.MZKoYmzzz9uUQVm3dY9Cqfq-4A4TyzE3Ot1WsONTPZQ" \
 -d '{
   "title": "Título Atualizado",
   "description": "Descrição Atualizada",
